f14adb143c414c7a4ee9cf121111b95b3c3f8436
j-bitanga <jbitanga88@gmail.com>
2021-12-14 21:43:47 -0800

    Initial commit

---
+++ src/index.js
@@ -0,0 +1,14 @@
+  
+import React from 'react'
+import ReactDOM from 'react-dom'
+import App from './App.js'
+import axios from 'axios'
+
+axios
+    .get('http://localhost:3001/notes')
+    .then (response => {
+    const notes = response.data
+    ReactDOM.render(<App notes={notes} />, document.getElementById('root'))
+    console.log(notes)
+})
+
